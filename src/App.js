002425c31c09c5659cf1fedbe517436161e28c4f
musmax <musaojo1710@yahoo.com>
2023-09-12 04:17:39 +0100

    added another exampl of api consumption

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [catFact, setCatFact] = useState("");
 
-  useEffect(() => {
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.length);
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
+
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredictedAge(res.data);
     })
-  }, [])
-
-
-
-
+  }
 
   return (
     <div className="App">
-      <button>Generate cat fact</button>
-      <p>{catFact}</p>
+      <input placeholder="Ex. Pedro..." onChange={(e) => {setName(e.target.value)}}/>
+      <button onClick={fetchData}>Predict User age</button>
+      <h1>Predicted Age: {predictedAge?.age} </h1>
+      <h1>Count: {predictedAge?.count} </h1>
+      <h1>Name: {predictedAge?.name} </h1>
     </div>
   );
 }
