c2ca139dc9ef88d4ae9245400ccf5d0e97b535c3
musmax <musaojo1710@yahoo.com>
2023-12-19 16:46:07 +0100

    chapter one learning

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,7 +2,7 @@ export const Profile = (props) => {
     return (
         <div>
             <h1>Hello user with name</h1> 
-         
+           <h3>Don't be a dullard oooo</h3>
         </div>
     )
 }
